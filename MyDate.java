9250725eda873d3301fbf28e276fa270bd798e70
marcoyimyaem <marcoyimyaem@gmail.com>
2023-10-28 00:11:25 +0800

    Create MyDate.java

---
+++ MyDate.java
@@ -0,0 +1,11 @@
+package Task121;
+import Task121.DateTask;
+public class MyDate {
+    public static void main(String[] args) {
+        DateTask date1 = new DateTask();
+        DateTask date2 = new DateTask();
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+       // display leapyears
+    }
+}
